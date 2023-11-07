6e474af295073817fc27f1323596545a2bb871d7
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2023-11-07 12:49:33 -0300

    #14 Add lint script and update form validation messages

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -11,7 +11,6 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
-  console.log(invoice);
 
   if (!invoice) {
     notFound();
